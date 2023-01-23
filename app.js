c5b2603fe69b8763dadab25ca83bbef7e7d08a2e
Daniel Romano <bydanielromano@gmail.com>
2023-01-23 17:32:04 +0100

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-//const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 const Prismic = require("@prismicio/client");
 const PrismicH = require("@prismicio/helpers");
 
