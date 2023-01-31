5ee9a0d870970e1bfd488b0fa357cbe013b9e45c
Daniel Romano <bydanielromano@gmail.com>
2023-01-31 17:22:33 +0100

    hopefully fixed the api call to prismic, the issue was in the route array passed in the creation of the prismic client, still have to figure out the right way to do it

--- app.js
+++ app.js
@@ -9,13 +9,30 @@ const fetch = require("node-fetch");
 const Prismic = require("@prismicio/client");
 const PrismicH = require("@prismicio/helpers");
 
-const initApi = (req) => {
-    return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
-      accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-      req,
-      fetch,
-    });
-};
+const routes = [
+  {
+    type: "page",
+    path: "/"
+  },{
+    type: "page",
+    path: "/about"
+  },{
+    type: "page",
+    path: "/collections"
+  },{
+    type: "page",
+    path: "/detail/:id"
+  },
+]
+
+const initApi = async (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    // routes,
+    req,
+    fetch
+  });
+}
 
 // const handleLinkResolver = (doc) => {
 //     if (doc.type === 'product') {
@@ -34,34 +51,38 @@ const initApi = (req) => {
 //     return '/';
 // };
 
-// app.use((req, res, next) => {
-//     res.locals.ctx = {
-//         endpoint: process.env.PRISMIC_ENDPOINT,
-//         linkResolver: handleLinkResolver
-//     };
-//     res.locals.PrismicDOM = PrismicDOM;
-//     next();
-// });
-
 app.use((req, res, next) => {
-    res.locals.ctx = {
-      PrismicH,
-    }
-    next()
+  res.locals.ctx = {
+    PrismicH,
+  }
+  next()
 })
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.get('/', async (req, res) => {
-    // Here we are retrieving the first document from your API endpoint
-    const document = await initApi();
-    console.log(document, "from api call");
-    res.render('pages/home', { document })
-})
+app.get('/', (req, res) => {
+  initApi(req).then((api) => {
+    api.get({
+      predicates: Prismic.predicate.at("document.type", "home")
+    }).then((response) => {
+      res.render('pages/home', { 
+        document: response.results[0]
+      });
+    })
+  });
+});
 
 app.get('/about', (req, res) => {
-    res.render("pages/about");
+  initApi(req).then((api) => {
+    api.get({
+      predicates: Prismic.predicate.at("document.type", "about")
+    }).then((response) => {
+      res.render('pages/about', { 
+        document: response.results[0]
+      });
+    })
+  });
 });
 
 app.get('/collections', (req, res) => {
